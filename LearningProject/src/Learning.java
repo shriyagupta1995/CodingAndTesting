64eeb10fd8ba607ca8e5ba9acc5a16677233b502
Shriya Gupta <shriya.gupta7014@gmail.com>
2026-03-25 22:24:33 +0530

    My first commit

---
+++ LearningProject/src/Learning.java
@@ -0,0 +1,23 @@
+
+public class Learning {
+
+	public static void main(String[] args) {
+		String name = "Shriya Gupta";
+		System.out.println("My name is "+name);
+		int a=12;
+		int b=3;
+		System.out.println(a*b);
+		System.out.println(a/b);
+		float c=1.1f;
+		System.out.println((c*b)+a);
+		char d='I';
+		boolean e= true;
+		
+		if(e) {
+			System.out.println(d+" "+name);
+		}
+		
+
+	}
+
+}
