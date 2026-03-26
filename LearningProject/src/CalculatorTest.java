a0deb531a6671573744391d3e4bcf5fdcb41bb86
Shriya Gupta <shriya.gupta7014@gmail.com>
2026-03-26 14:36:15 +0530

    Main.java

---
+++ LearningProject/src/CalculatorTest.java
@@ -0,0 +1,44 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+//TDD is shown in this example as I am writing unit tests for calculator and shapes class
+class CalculatorTest {
+
+	@Test
+	public void testAddition() {
+		Calculator calc = new Calculator();
+		Shapes sh = new Shapes();
+		assertEquals(5, calc.add(2, 3));
+		assertEquals(10, calc.add(5, 3, 2));
+		assertEquals(16, sh.area(4));
+		assertEquals(12, sh.area(3,4));
+		//fail("Not yet implemented");
+	}
+	
+	//This class also shows method overloading in action as I have created 2 methods
+	//with same name
+	public class Calculator{
+		public int add(int a, int b) {
+			return a+b;
+		}
+		public int add(int c, int d, int e) {
+			return c+d+e;
+		}
+	}
+	
+	//This class also shows method overloading in action as I have created 2 methods
+	//with same name
+	public class Shapes{
+		//To calculate area of square
+		public int area(int s) {
+			return s*s;
+		}
+		
+		// To calculate area of rectangle
+		public int area(int l, int b) {
+			return l*b;
+		}
+	}
+
+}
---
+++ LearningProject/src/Main.java
@@ -0,0 +1,55 @@
+//This example shows method overriding
+
+public class Main {
+
+	public static void main(String[] args) {
+		String q = null;
+		Vehicle v1= new Car();
+		Car c = new Car();
+		v1.displayInfo();
+		c.displayInfo(q);
+		Vehicle v2 = new Bike();
+		v2.displayInfo();
+		Vehicle v3 = new Bus();
+		v3.displayInfo();
+
+	}
+
+}
+
+class Vehicle{
+	public void displayInfo(){
+		System.out.println("Some vehicles are there");
+		
+	}
+}
+
+class Car extends Vehicle{
+
+	@Override
+	public void displayInfo() {
+		System.out.println("I have a car");
+	}
+	
+	public void displayInfo(String v) {
+		v = "I have a truck";
+		System.out.println(v);
+	}
+	
+}
+class Bike extends Vehicle{
+
+	@Override
+	public void displayInfo() {
+		System.out.println("I have a Bike");
+	}
+	
+}
+class Bus extends Vehicle{
+
+	@Override
+	public void displayInfo() {
+		System.out.println("I have a Bus");
+	}
+	
+}
